702371dc38d6b9c6cae84db1bd338d3220d181c3
Nick Pantev <103816194+IYI-Pantev@users.noreply.github.com>
2025-06-22 18:36:01 +0300

    types in props and hooks

---
+++ src/components/Person.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+interface PersonProps {
+  name: string;
+  age: number;
+  isMarried: boolean;
+}
+
+const Person = ({ name, age, isMarried }: PersonProps) => {
+  const [isShowDetails, setIsShowDetails] = useState<boolean>(false);
+  const [personBio, setPersonBio] = useState<string | null>(null);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPersonBio(event.target.value);
+  };
+  return (
+    <div>
+      <h2>{name}</h2>
+      <button onClick={() => setIsShowDetails(!isShowDetails)}>
+        {isShowDetails ? "Hide Details" : "Show Details"}
+      </button>
+      {isShowDetails && (
+        <>
+          <p>Age: {age}</p>
+          <p>Married: {isMarried ? "Yes" : "No"}</p>
+        </>
+      )}
+      <p>
+        {name} Bio: {!personBio ? "No Bio available" : personBio}
+      </p>
+      <input type="text" onChange={handleChange} />
+    </div>
+  );
+};
+
+export default Person;
